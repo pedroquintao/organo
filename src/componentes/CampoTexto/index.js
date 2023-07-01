4d91571dc982a0278ce11c4b76bb0e24f8d078e6
pedroquintao <pedro.h.quintao@hotmail.com>
2023-06-30 21:25:27 -0300

    Atividade 02 da aula 03 finalizada

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -4,10 +4,17 @@ const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder}...` //não é obrigatório, mas é normal criar constantes para adicionar como propriedades
 
+    let valor = ''
+
+    const aoDigitado = (e) => {
+        valor = e.target.value
+        console.log("Valor=", valor)
+    }
+
     return (
         <div className="campo-texto">   
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={placeholderModificada}/>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
